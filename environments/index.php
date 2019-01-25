16adbc25600aefdc5612cb5f02a48309ada6aa15
paul <gogolinsky.pavel@gmail.com>
2019-01-26 02:20:38 +0300

    Fix env files

--- environments/index.php
+++ environments/index.php
@@ -32,34 +32,20 @@ return [
     'Development' => [
         'path' => 'dev',
         'setWritable' => [
-            'backend/runtime',
-            'backend/web/assets',
-            'frontend/runtime',
-            'frontend/web/assets',
+            'api/runtime',
         ],
         'setExecutable' => [
             'yii',
             'yii_test',
         ],
-        'setCookieValidationKey' => [
-            'backend/config/main-local.php',
-            'frontend/config/main-local.php',
-        ],
     ],
     'Production' => [
         'path' => 'prod',
         'setWritable' => [
-            'backend/runtime',
-            'backend/web/assets',
-            'frontend/runtime',
-            'frontend/web/assets',
+            'api/runtime',
         ],
         'setExecutable' => [
             'yii',
-        ],
-        'setCookieValidationKey' => [
-            'backend/config/main-local.php',
-            'frontend/config/main-local.php',
         ],
     ],
 ];
